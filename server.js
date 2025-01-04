2bd4a85cdcb94c9d6528ca5f5853c5cb9a767df7
Nakry Mut <nakrymut375@gmail.com>
2025-01-04 16:33:42 +0700

    pdate

--- server.js
+++ server.js
@@ -15,14 +15,13 @@ app.use(express.json());  // Parses JSON data
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverSelectionTimeoutMS: 30000 // 30 seconds
-})
-.then(() => console.log("Connected to MongoDB"))
-.catch((err) => {
-  console.error("Error connecting to MongoDB:", err);
-});
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 50000, // 50 seconds
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Error connecting to MongoDB:", err));
+  
 
 // Routes
 app.use("/api/category", categoryRoute);
